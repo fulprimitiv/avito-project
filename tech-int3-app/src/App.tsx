cd8ffc5ae06221ca31b327a3d2537e9e2f077b7e
fulprimitiv <eremeevdima51@gmail.com>
2025-11-24 02:54:53 +0500

    feat: роутинг, верстка header

--- tech-int3-app/src/App.tsx
+++ tech-int3-app/src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
+import Header from './components/Header/Header';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ListPage from './pages/ListPage/ListPage';
+import ItemPage from './pages/ItemPage/ItemPage';
+import StatsPage from './pages/StatsPage/StatsPage';
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="app-wrapper">
+      <Header />
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/list" replace />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/item/:id" element={<ItemPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
