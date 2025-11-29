d4ea02a5d5fb419e836ea1d1c24ff796e56c769d
fulprimitiv <eremeevdima51@gmail.com>
2025-11-29 11:27:50 +0500

    feat: навигация к /stats

--- tech-int3-app/src/components/global-components/Header/Header.tsx
+++ tech-int3-app/src/components/global-components/Header/Header.tsx
@@ -13,7 +13,9 @@ export const Header: React.FC = () => (
                className="app-header__logo"
             />
          </Link>
-         <span className="app-header__author-label">Еремеев Дмитрий Андреевич</span>
+         <Link to="/stats" className="app-header__author-link">
+            <span className="app-header__author">Еремеев Дмитрий Андреевич</span>
+         </Link>
       </div>
    </header>
 );
